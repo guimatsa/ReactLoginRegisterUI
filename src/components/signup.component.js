b432c9976e103eb59922e9a62525368e43b8bcf9
julio guimatsa <julio.guimatsa@insightahaed.com>
2021-10-30 00:36:46 +0100

    second commit

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -4,17 +4,8 @@ export default class SignUp extends Component {
     render() {
         return (
             <form>
-                <h3>Register</h3>
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+                <h3>Log in</h3>
 
                 <div className="form-group">
                     <label>Email</label>
@@ -26,9 +17,16 @@ export default class SignUp extends Component {
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">log in?</a>
+                    Forgot <a href="#">password?</a>
                 </p>
             </form>
         );
