1b119146107e87604b9a5934830f3f76553b8109
julio guimatsa <julio.guimatsa@insightahaed.com>
2021-10-30 00:47:00 +0100

    tirth commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
 import SignIn from './components/signIn.component';
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <div className="outer">
-        <div className="inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/sign-in" component={SignIn} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
+  return (
+    <Router>
+      <div className="App">
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/login" component={Login} />
+              <Route path="/SignUp" component={SignIn} />
+              <Route path="/SignIn" component={SignUp} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div></Router>
+    </Router>
   );
 }
 
--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,33 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default class SignUp extends Component {
+export default class SignIn extends Component {
     render() {
         return (
             <form>
-
-                <h3>Log in</h3>
-
+                <h2>Microsoft</h2>
                 <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label className="sign_in">Sign in</label>
+                    <input type="email" className="form-control" placeholder="Email, Phone or Skipe" />
                 </div>
-
                 <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <p class="create_account"> No account?<Link> <a>   Create one! </a> </Link> </p>
                 </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
+                <button type="button" class="btn btn-primary float-right">Next</button>
             </form>
         );
     }
