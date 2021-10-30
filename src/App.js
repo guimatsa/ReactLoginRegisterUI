1ae3fc9c40cf2b85eaea646fce5bc71a2275aa05
julio guimatsa <julio.guimatsa@insightahaed.com>
2021-10-30 12:09:53 +0100

    commeit 6 at 12:09 am

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/email.component";
-import CountrySelector from './components/birthdate.component';
+import Birthdate from './components/birthdate.component';
 import Email from './components/email.component';
 import Password from './components/password.component'
 import Name_Sname from './components/name_sname.component'
@@ -17,8 +17,8 @@ function App() {
           <div className="inner">
             <Switch>
               <Route exact path='/Email' component={Email} />
-              <Route exact path='/CountrySelector' component={CountrySelector} />
               <Route exact path='/Password' component={Password} />
+              <Route exact path='/Birthdate' component={Birthdate} />
               <Route exact path='/Name_Sname' component={Name_Sname} />
               
             </Switch>
--- src/components/birthdate.component.js
+++ src/components/birthdate.component.js
@@ -1,16 +1,53 @@
-import React, { useState, useMemo } from 'react'
-import Select from 'react-select'
-import countryList from 'react-select-country-list'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-function CountrySelector() {
-  const [value, setValue] = useState('')
-  const options = useMemo(() => countryList().getData(), [])
+export default class Birthdate extends Component {
+  render() {
+    return (
+      <form>
+        <h2>Microsoft</h2>
+        <div className="form-group">
+          <p class="create_account "> User Email</p>
+          <label className="sign_in">What's your birthdate?</label>
+          <p class="create_account"> We need just a little more info to set up your account. Your date of birth helps us to provide you with age-appropriate settings. </p>
 
-  const changeHandler = value => {
-    setValue(value)
+          <label className="sign_in">Country/region</label> <br />
+
+          <select class="form-control" aria-label=".form-select-lg example">
+            <option selected>Select your country</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select> <br />
+          <label className="sign_in">Birthdate</label> <br />
+          <div className="testd d-inline p-1">
+          <select class="form-control-sub  position-relative" aria-label=".form-select-lg example">
+            <option selected>Month</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          </div>
+          <div className="testd d-inline p-1">
+          <select class="form-control-sub  position-relative" aria-label=".form-select-lg example">
+            <option selected>Day</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          </div>
+          <div className="testd d-inline p-1">
+          <select class="form-control-sub  position-relative" aria-label=".form-select-lg example">
+            <option selected>Year</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          </div>
+        </div>
+       
+        <button type="button" class="btn btn-primary float-right">Next</button>
+      </form>
+    );
   }
-
-  return <Select options={options} value={value} onChange={changeHandler} />
 }
-
-export default CountrySelector
